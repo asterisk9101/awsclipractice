215a76339cccc8f28d8c6bc5a749c6959eb19dff
asterisk9101 <tm11b028@gmail.com>
2024-01-01 23:43:07 +0900

    del

--- bin/restore-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ueo pipefail
-
-BackupVaultName="$1"
-RecoveryPointArn="$2"
-
-aws backup delete-recovery-point --backup-vault-name "$BackupVaultName" --recovery-point-arn "$RecoveryPointArn"
-
-exit 0
