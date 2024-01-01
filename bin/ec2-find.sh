269c13abe23b414a6ece79de5409ab69fa898f3a
asterisk9101 <tm11b028@gmail.com>
2024-01-01 14:55:38 +0900

    add ec2-find.sh

---
+++ bin/ec2-find.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ueo pipefail
+Instances=$(aws ec2 describe-instances --filters "$@" | jq -c ".Reservations[]")
+headers="tag:Name                       instance-id  state       instance-type launch-time platform-details"
+select="(.Tags | from_entries | .Name),.InstanceId, .State.Name,.InstanceType,.LaunchTime,.PlatformDetails"
+query=".Instances[] | [$select] | @tsv"
+cat <(echo "$headers"; echo "$Instances" | jq -c -r "$query") | column -t
+exit 0
