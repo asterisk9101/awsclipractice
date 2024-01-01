da944b1befe43bd63aa7bd34dcdfcdb4427d0226
asterisk9101 <tm11b028@gmail.com>
2024-01-01 22:06:53 +0900

    fix restore-find.sh

--- bin/restore-find.sh
+++ bin/restore-find.sh
@@ -5,7 +5,7 @@ NAME="$1"
 
 list=$(aws backup list-recovery-points-by-backup-vault --backup-vault-name "$NAME")
 header="RecoveryPointArn   Status ResourceId                     ResourceType  CreationDate  DeleteAt"
-select='.RecoveryPointArn,.Status,(.ResourceArn | sub(".*:";"") ),.ResourceType,.CreationDate,.CalculatedLifecycle.DeleteAt'
+select='.RecoveryPointArn,.Status,(.ResourceArn | sub(".*:";"") ),.ResourceType,.CreationDate[0:19],.CalculatedLifecycle.DeleteAt[0:19]'
 query=".RecoveryPoints[] | [$select] | @tsv"
 cat <(echo $header; echo $list | jq -r "$query") | column -t
 
