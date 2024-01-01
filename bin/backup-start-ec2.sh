eb7e36ed6c4f89522c7683994eae4e77d769611d
asterisk9101 <tm11b028@gmail.com>
2024-01-01 20:18:20 +0900

    fix backup-start-ec2.sh

--- bin/backup-start-ec2.sh
+++ bin/backup-start-ec2.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+set -ueo pipefail
 
 NAME="$1"
 VaultName="$NAME"
 
 Instances=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$NAME" | jq -c .Reservations[])
+InstanceId=$(echo "$Instances" | jq .Instances[].InstanceId)
 Region=$(echo "$Instances" | jq -r .Instances[].Placement.AvailabilityZone | sed 's/.$//')
 AccountId=$(echo "$Instances" | jq -r .OwnerId)
 ResourceArn=arn:aws:ec2:${Region}:${AccountId}:instance/${InstanceId}
 IamRoleArn=arn:aws:iam::${AccountId}:role/service-role/AWSBackupDefaultServiceRole
 CompleteWindowMinutes=1440 # 指定した時間以内に完了しなければキャンセル（Expire）
 Lifecycle=DeleteAfterDays=1 # バックアップの削除
+
+echo "Backup Params"
+cat <(
+echo "VaultName: $VaultName"
+echo "IamRoleArn: $IamRoleArn"
+echo "ResourceArn: $ResourceArn"
+echo "CompleteWindowMinutes: $CompleteWindowMinutes"
+echo "LifeCycle: $Lifecycle"
+) | column -t
 
 Job=$(aws backup start-backup-job \
     --backup-vault-name "$VaultName" \
