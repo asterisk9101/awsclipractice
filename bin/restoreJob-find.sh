1d22717f686cf3f46b0e12d99595665724846b6d
asterisk9101 <tm11b028@gmail.com>
2024-01-01 23:44:04 +0900

    fix resotreJob-find.sh

--- bin/restoreJob-find.sh
+++ bin/restoreJob-find.sh
@@ -6,7 +6,7 @@ DATE="${1:-yesterday}"
 RestoreJobs=$(aws backup list-restore-jobs --by-created-after $(date "+%FT%TZ" --utc -d "$DATE"))
 
 headers="RestoreJobId,Status,PercentDone,CreatedResourceArn,ResourceType,CreationDate"
-select='.RestoreJobId,.Status,.PercentDone,(.CreatedResourceArn | sub(".*:";"")),.ResourceType,(.CreationDate[0:19])'
+select='.RestoreJobId,.Status,.PercentDone,.CreatedResourceArn,.ResourceType,(.CreationDate[0:19])'
 query=".RestoreJobs[] | [$select] | @csv"
 
 cat <(echo "$headers"; echo "$RestoreJobs" | jq -c -r "$query") | tr -d '"' | column -t -s ,
