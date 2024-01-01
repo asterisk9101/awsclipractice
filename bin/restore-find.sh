eddfb9a83a645990260e3853492ef7d3347f33c8
asterisk9101 <tm11b028@gmail.com>
2024-01-01 21:59:04 +0900

    fix restore-find.sh

--- bin/restore-find.sh
+++ bin/restore-find.sh
@@ -4,8 +4,8 @@ set -ueo pipefail
 NAME="$1"
 
 list=$(aws backup list-recovery-points-by-backup-vault --backup-vault-name "$NAME")
-header="BackupVaultName   ResourceType  RecoveryPointArn  CreationDate  DeleteAt"
-select=".BackupVaultName,.ResourceType,.RecoveryPointArn,.CreationDate,.CalculatedLifecycle.DeleteAt"
+header="RecoveryPointArn   Status  BackupVaultName   ResourceId                     ResourceType  CreationDate  DeleteAt"
+select='.RecoveryPointArn,.Status,.BackupVaultName,(.ResourceArn | sub(".*:";"") ),.ResourceType,.CreationDate,.CalculatedLifecycle.DeleteAt'
 query=".RecoveryPoints[] | [$select] | @tsv"
 cat <(echo $header; echo $list | jq -r "$query") | column -t
 
