3d2840b77ba24f4f675e58863fe0ff4cc24c5498
asterisk9101 <tm11b028@gmail.com>
2024-01-01 21:20:12 +0900

    fix backup-find.sh

--- bin/backup-find.sh
+++ bin/backup-find.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ueo pipefail
 
-BackupJobs=$(aws backup list-backup-jobs --by-created-after $(date --utc +%F))
+FORMAT="${1:-%F}"
+
+BackupJobs=$(aws backup list-backup-jobs --by-created-after $(date --utc +$FORMAT))
 
 headers="BackupJobId,State,BackupVaultName,ResourceArn,StatusMessage,ResourceType,CreationDate"
 select='.BackupJobId,.State,.BackupVaultName,(.ResourceArn | sub(".*:";"")),.StatusMessage,.ResourceType,(.CreationDate[0:19] | sub("T";" "))'
