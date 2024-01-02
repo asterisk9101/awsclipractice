2c634895a920c5151ff9d391bfcf406b271ed7e5
asterisk9101 <tm11b028@gmail.com>
2024-01-02 07:25:20 +0000

    restore-job-list

---
+++ bin/restore-job-list.jq
@@ -0,0 +1,8 @@
+{
+    "RestoreJobId": .RestoreJobId,
+    "Status": .Status,
+    "PercentDone": .PercentDone,
+    "CreatedResourceArn": .CreatedResourceArn,
+    "ResourceType": .ResourceType,
+    "CreationDate": (.CreationDate[0:19])
+}
---
+++ bin/restore-job-list.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ueo pipefail
+
+DATE="${1:-24} hours ago"
+CreatedAfter=$(date "+%FT%TZ" --utc -d "$DATE")
+
+Jobs=$(aws backup list-restore-jobs --by-created-after "$CreatedAfter" | jq -c .RestoreJobs[])
+
+queryfile="$(dirname $0)/$(basename -s .sh $0).jq"
+table=$(echo "$Jobs" | jq -c -r -f "$queryfile")
+
+if [ -z "$table" ]; then
+    echo "Job Not Found"
+    exit 1
+fi
+
+keys=$(echo "$table" | jq -s -r -c ".[0] | keys" | sed -e 's/\[/[./' -e 's/,/,./g')
+cat <(
+    echo "$table" | jq -s -r ".[0] | keys | @csv"
+    echo "$table" | jq -r "$keys | @csv"
+) | tr -d '"' | column -s , -t
+
+exit 0
