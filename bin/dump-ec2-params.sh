ffde08e7dc06031820f3cd9a34f7ad7c83e38de5
asterisk9101 <tm11b028@gmail.com>
2024-01-14 14:21:55 +0000

    fix dump src by instance id

--- bin/dump-ec2-params.sh
+++ bin/dump-ec2-params.sh
@@ -4,7 +4,7 @@ set -ueo pipefail
 # 関数定義
 function format(){
     # info または error から呼び出し
-    echo "$(date '+%F %T') $(basename "$0") $PROFILE $NameTag $1 $2"
+    echo "$(date '+%F %T') $(basename "$0") $PROFILE $InstanceId $1 $2"
 }
 
 function info(){
@@ -17,32 +17,35 @@ function error(){
 }
 
 PROFILE="$1"
-NameTag="$2"
-info "PROFILE=$PROFILE"
-info "TagName=$NameTag"
+InstanceId="$2"
 
 # インスタンスの検索
-Instances=$(aws --profile "$PROFILE" ec2 describe-instances --filters "Name=tag:Name,Values=$NameTag")
+Instances=$(aws --profile "$PROFILE" ec2 describe-instances --filters "Name=instance-id,Values=$InstanceId")
 
 # インスタンスIDの取得
 InstanceId=$(echo "$Instances" | jq -r .Reservations[].Instances[].InstanceId)
 
-if [ -z "$InstanceId" ]; then error "インスタンスが見つかりません： $NameTag"; fi
-if [ "$(echo "$InstanceId" | wc -l)" -gt 1 ]; then error "インスタンスが複数見つかりました: $InstanceId"; fi
+if [ -z "$InstanceId" ]; then
+    error "インスタンスが見つかりません： $InstanceId";
+fi
+if [ "$(echo "$InstanceId" | wc -l)" -gt 1 ]; then
+    error "インスタンスが複数見つかりました: $InstanceId";
+fi
+if [ "$(echo "$Instances" | jq -r .Reservations[].Instances[].State.Name)" == "terminated" ]; then
+    error "インスタンスが終了しています: $InstanceId"
+fi
 
-info "InstanceID=$InstanceId"
-
-echo "EC2インスタンスの情報をファイルに出力します: ${InstanceId}.json"
+info "EC2インスタンスの情報をファイルに出力します: ${InstanceId}.json"
 echo "$Instances" | jq . > "${InstanceId}.json"
 
-echo "EC2インスタンスに紐づくEBSの情報をファイルに出力します: ${InstanceId}-ebs.json"
+info "EC2インスタンスに紐づくEBSの情報をファイルに出力します: ${InstanceId}-ebs.json"
 VolumeIds=$(echo "$Instances" | jq -r .Reservations[].Instances[].BlockDeviceMappings[].Ebs.VolumeId | tr '\n' ' ')
 Volumes=$(aws ec2 describe-volumes --volume-ids $VolumeIds)
-echo $Volumes | jq . > "${InstanceId}-EBS.json"
+echo $Volumes | jq . > "${InstanceId}-ebs.json"
 
-echo "EC2インスタンスに紐づくENIの情報をファイルに出力します: ${InstanceId}-eni.json"
+info "EC2インスタンスに紐づくENIの情報をファイルに出力します: ${InstanceId}-eni.json"
 InterfaceIds=$(echo "$Instances" | jq -r .Reservations[].Instances[].NetworkInterfaces[].NetworkInterfaceId | tr '\n' ' ')
 Interfaces=$(aws ec2 describe-network-interfaces --network-interface-ids $InterfaceIds)
-echo $Interfaces | jq . > "${InstanceId}-ENI.json"
+echo $Interfaces | jq . > "${InstanceId}-eni.json"
 
 exit 0
