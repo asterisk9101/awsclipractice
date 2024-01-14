aa0bae8be41021a6debe705030d7eb2b1a772372
asterisk9101 <tm11b028@gmail.com>
2024-01-14 05:35:01 +0000

    add color scheme

--- bin/table.sh
+++ bin/table.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
 # 標準入力がパイプ(0ではない)なら表敬式に整形して出力する
 if [ ! -t 0 ]; then
     
@@ -8,9 +11,12 @@ if [ ! -t 0 ]; then
 
     keys=$(echo "$table" | jq -s -r -c ".[0] | keys" | sed -e 's/\[/[./' -e 's/,/,./g')
     cat <(
+        echo -ne '\e[1;34m' # blue bold
         echo "$table" | jq -s -r ".[0] | keys | @csv"
+        echo -ne '\e[0;32m' # green regular
         echo "$table" | jq -r "$keys | @csv"
     ) | tr -d '"' | column -s , -t
+    echo -ne '\e[0m' # color reset
 fi
 
 exit 0
